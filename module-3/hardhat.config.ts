da1c7aa3d44ad22c06913df4d6650c42a58cceda
basonpark <bp2470@columbia.edu>
2025-02-02 17:33:34 +0900

    final commit before meeting

--- module-3/hardhat.config.ts
+++ module-3/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -7,13 +8,12 @@ dotenv.config();
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
-    mumbai: {
-      url: process.env.MUMBAI_RPC_URL,
+    polygonAmoy: {
+      url: process.env.POLYGON_RPC_URL,
       accounts:
         process.env.METAMASK_PRIVATE_KEY !== undefined
           ? [process.env.METAMASK_PRIVATE_KEY]
           : [],
-          gasPrice: 25000000000,
     },
   },
 };
--- module-3/scripts/deploy.ts
+++ module-3/scripts/deploy.ts
@@ -1,8 +1,22 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import "@nomiclabs/hardhat-ethers";
 const hre = require("hardhat");
+import { ethers } from "ethers"
 
 async function main() {
+
+  // const [deployer] = await ethers.getSigners(); // Get the deployer account  
+  // console.log("Deployer:", deployer);
+  // const balance = await ethers.provider.getBalance(deployer.address); // Check balance  
+  // console.log("Account balance:", ethers.formatEther(balance), "ETH");  
+  
+  //question for instructor: I am having trouble deploying this contract.
+  //I've compiled the contracts well and set the right configs for polygonAmoy and sufficient funds in wallet
+  //The main trouble is wthe conflict with packages (ethers from hardhat, ethers, nomiclabs, nomicfoundation, hardhat-ethers, hardhat-toolbox)
+  //I know it should be easy but I've been stuck on this for a long time and I can use some suport.
+  //Also, package.json is not showing all the devDependencies and dependencies.
+  //I've tried to install all the dependencies but it didn't work, the the version control is getting messy.
+
   const TokenFactory = await hre.ethers.getContractFactory("Token");
   const ForgingLogicFactory = await hre.ethers.getContractFactory(
     "ForgingLogic"
@@ -12,12 +26,8 @@ async function main() {
 
   //deploy Token contract with a fixed gasLimit
   console.log("Deploying Token contract...");
-  const token = await TokenFactory.deploy({
-    gasLimit: 500000,
-    gasPrice: hre.ethers.utils.parseUnits('30', 'gwei'),
-  });
-  console.log("Waiting for Token deployment...");
-  await token.waitForDeployment();
+  const token = await TokenFactory.deploy();
+  await token.deployed();
 
   console.log("Token:", token); 
   console.log("Token deployed to:", token.address);
