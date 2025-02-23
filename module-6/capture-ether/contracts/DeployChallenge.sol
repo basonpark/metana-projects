ed697eb6416cf90decba90bdc08261616fc3c499
basonpark <bp2470@columbia.edu>
2025-02-23 16:15:18 +0900

    restarting capture the ether module-6

--- module-6/capture-ether/contracts/DeployChallenge.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.21;
-
-contract DeployChallenge {
-    // This tells the CaptureTheFlag contract that the challenge is complete.
-    function isComplete() public pure returns (bool) {
-        return true;
-    }
-}
