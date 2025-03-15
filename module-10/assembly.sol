af2c4d460aa67ac9bf7cdc40a29bba0c0eeea42d
basonpark <bp2470@columbia.edu>
2025-03-15 18:23:29 +0900

    charAt in assembly

--- module-10/assembly.sol
+++ module-10/assembly.sol
@@ -16,4 +16,30 @@ contract BitWise {
         }  
         }  
     }  
+}   
+
+contract String {
+    function charAt(string memory input, uint index) public pure returns(bytes2) {  
+        assembly {  
+        let strLen := mload(input)  
+        
+        // check if index is out of bounds / string is empty
+        if or(iszero(strLen), gte(index, strLen)) {  
+            return(0, 0x20)  
+        }  
+        
+        // calculate memory position of character, and add index to that position
+        let charPos := add(add(input, 0x20), index)  
+        
+        // load the word containing character  
+        let data := mload(charPos)  
+        
+        // keep only the first 2 bytes and return
+        data := shl(0xe0, data)  // shift left by 224 bits (256-16=240 bits)  
+        data := shr(0xe0, data)  // shift right to get back to position 0  
+        
+        mstore(0, data)  
+        return(0, 0x20)  
+        }  
+    }  
 }  
