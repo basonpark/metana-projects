706f184661ae5ea4890d5796d8df43444ba605db
basonpark <bp2470@columbia.edu>
2025-06-04 06:51:49 +0900

    module-4

---
+++ module-4/module-3-test/client/next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'ipfs.io',
+      port: '',
+      pathname: '/ipfs/**',
+    },
+    ],
+  },
+};
+
+export default nextConfig;
